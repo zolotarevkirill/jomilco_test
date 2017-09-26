7cce36ef7c6a54522697cf291267ceb6b2cf06d4
Zolotarev.K <zolotarevkirill@yandex.ru>
2017-09-26 12:29:21 +0300

    +sort +train list +select position train +decorator for select

--- index.php
+++ index.php
@@ -202,6 +202,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="/js/template.js"></script>
+<script src="/build/js/template.js"></script>
 </body>
 </html>
